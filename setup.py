f6a0625b2991b3492cd811d0d71ea48fb311e532
Syrine <syrinetriter@outlook.com>
2025-11-20 15:21:50 +0100

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Syrine Triter",
+    author_email="syrinetriter@outlook.com",
+    packages=find_packages(),
+    install_requires=[]
+)
