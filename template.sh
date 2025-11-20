c35b7bb387a69afe90db07d7184e2251b0c55687
Syrine Triter <syrinetr2004@gmail.com>
2025-11-20 13:33:38 +0100

    folder structure added

---
+++ template.sh
@@ -0,0 +1,15 @@
+#create directories
+mkdir -p src
+mkdir -p research
+
+#creating files
+touch src/__init__.py
+touch src/helpers.py
+touch src/prompts.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "directories and files created successfully"
